75722d44fcb9354eb5753b25864169f0f6a2e13a
Alex Hilton <dragonhilton@gmail.com>
2015-07-28 00:35:42 +0800

    Add sun renderer
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

---
+++ app/src/main/java/net/toughcoder/opengl2s/SunRenderer.java
@@ -0,0 +1,29 @@
+package net.toughcoder.opengl2s;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by alexhilton on 15/7/28.
+ *
+ * This is the two dimension example.
+ * A shinny sun, with lights.
+ */
+public class SunRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
