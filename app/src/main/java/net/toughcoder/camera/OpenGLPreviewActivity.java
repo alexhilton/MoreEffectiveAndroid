0acd300002f33338071f988aec9701d6277becfb
Alex Hilton <dragonhilton@gmail.com>
2016-05-29 22:52:35 +0800

    Add the opengl preview renderer
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/camera/OpenGLPreviewActivity.java
+++ app/src/main/java/net/toughcoder/camera/OpenGLPreviewActivity.java
@@ -1,5 +1,6 @@
 package net.toughcoder.camera;
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
@@ -7,11 +8,37 @@ import net.toughcoder.effectiveandroid.R;
 
 public class OpenGLPreviewActivity extends ActionBarActivity {
     private static String TAG = "OpenGL Preview";
+    private GLSurfaceView mSurfaceView;
+    private OpenGLPreviewRenderer mRenderer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle(TAG);
         setContentView(R.layout.activity_open_glpreview);
+        mSurfaceView = (GLSurfaceView) findViewById(R.id.openglview);
+        mRenderer = new OpenGLPreviewRenderer();
+        setupSurfaceView(mRenderer);
+    }
+
+    private void setupSurfaceView(OpenGLPreviewRenderer renderer) {
+        mSurfaceView.setEGLContextClientVersion(2);
+        mSurfaceView.setPreserveEGLContextOnPause(true);
+        mSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        mSurfaceView.setRenderer(renderer);
+        mSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        mSurfaceView.requestRender();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSurfaceView.onResume();
     }
 }
---
+++ app/src/main/java/net/toughcoder/camera/OpenGLPreviewRenderer.java
@@ -0,0 +1,26 @@
+package net.toughcoder.camera;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by alexhilton on 16/5/29.
+ */
+public class OpenGLPreviewRenderer implements GLSurfaceView.Renderer {
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
