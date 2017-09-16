9f16352e5187c4c09d7cd5a0bcd8e92416c69646
Alex Hilton <dragonhilton@gmail.com>
2017-09-16 21:48:30 +0800

    Remove code duplicates.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/opengl/sharedcontext/SharedEGLContextActivity.java
+++ app/src/main/java/net/toughcoder/opengl/sharedcontext/SharedEGLContextActivity.java
@@ -5,9 +5,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Point;
 import android.graphics.SurfaceTexture;
-import android.hardware.Camera;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
@@ -33,9 +31,6 @@ import android.view.Surface;
 
 import net.toughcoder.effectiveandroid.R;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -297,122 +292,20 @@ public class SharedEGLContextActivity extends Activity implements SurfaceTexture
         mSurfaceTexture = new SurfaceTexture(mPreviewTexture);
     }
 
-    public static int loadProgram(final String vsh, final String fsh) {
-        int vshader = loadShader(vsh, GLES20.GL_VERTEX_SHADER);
-        if (vshader == 0) {
-            Log.e(TAG, "failed to load vertex shader " + vsh);
-            return 0;
-        }
-
-        int fshader = loadShader(fsh, GLES20.GL_FRAGMENT_SHADER);
-        if (fshader == 0) {
-            Log.e(TAG, "failed to load fragment shader " + fsh);
-            return 0;
-        }
-
-        int program = GLES20.glCreateProgram();
-        GLES20.glAttachShader(program, vshader);
-        GLES20.glAttachShader(program, fshader);
-        GLES20.glLinkProgram(program);
-
-        int[] status = new int[1];
-        GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, status, 0);
-        if (status[0] <= 0) {
-            Log.e(TAG, "failed to link program");
-        }
-
-        GLES20.glDeleteShader(vshader);
-        GLES20.glDeleteShader(fshader);
-
-        return program;
-    }
-
-    public static int loadShader(String source, int type) {
-        int[] status = new int[1];
-        int shader = GLES20.glCreateShader(type);
-        GLES20.glShaderSource(shader, source);
-        GLES20.glCompileShader(shader);
-        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, status, 0);
-        if (status[0] == 0) {
-            Log.e(TAG, "failed to compile shader " + source + ", type " + type);
-            return 0;
-        }
-        return shader;
-    }
-
-    private class PreviewRenderer implements GLSurfaceView.Renderer {
+    private class PreviewRenderer extends SurfaceTextureRenderer {
         private static final String TAG = "PreviewRenderer";
-        public final float[] CUBE = {
-                -1.0f, -1.0f,
-                1.0f, -1.0f,
-                -1.0f, 1.0f,
-                1.0f, 1.0f,
-        };
-        public final float[] TEXTURE_NO_ROTATION = {
-                0.0f, 0.0f,
-                1.0f, 0.0f,
-                0.0f, 1.0f,
-                1.0f, 1.0f,
-        };
-        private final FloatBuffer mCubeBuffer;
-        private final FloatBuffer mTextureBuffer;
-
-        public static final String VERTEX_SHADER =
-                "attribute vec4 position;\n" +
-                        "uniform mat4 uSTMatrix;\n" +
-                        "attribute vec4 inputTextureCoords;\n" +
-                        "varying vec2 textureCoords;\n" +
-                        "void main() {\n" +
-                        "  gl_Position = position;\n" +
-                        "  textureCoords = (uSTMatrix * inputTextureCoords).xy;\n" +
-                        "}";
-
-        public static final String FRAGMENT_SHADER =
-                "#extension GL_OES_EGL_image_external : require\n" +
-                        "precision highp float;\n" +
-                        "uniform samplerExternalOES uTextureSampler;\n" +
-                        "varying vec2 textureCoords;\n" +
-                        "void main () {\n" +
-                        "  vec4 tex = texture2D(uTextureSampler, textureCoords);\n" +
-                        "  gl_FragColor = vec4(tex.rgb, 1);\n" +
-                        "}";
-
-        private int mProgram;
-        private int mAttributePosition;
-        protected int mTextureCoords;
-        private int mUniformLocation;
-        private int mUniformMatrix;
-
-        public PreviewRenderer() {
-            mCubeBuffer = ByteBuffer.allocateDirect(CUBE.length * 4)
-                    .order(ByteOrder.nativeOrder())
-                    .asFloatBuffer();
-            mCubeBuffer.put(CUBE).position(0);
-
-            mTextureBuffer = ByteBuffer.allocateDirect(TEXTURE_NO_ROTATION.length * 4)
-                    .order(ByteOrder.nativeOrder())
-                    .asFloatBuffer();
-            mTextureBuffer.put(TEXTURE_NO_ROTATION).position(0);
-        }
 
         @Override
         public void onSurfaceCreated(GL10 gl, EGLConfig config) {
             Log.d(TAG, "onSurfaceCreated");
             initializeSurfaceTexture();
-
-            // Initialize GL stuff
-            GLES20.glDisable(GLES20.GL_DITHER);
-            GLES20.glClearColor(0, 0, 0, 0);
-            GLES20.glEnable(GLES20.GL_CULL_FACE);
-            GLES20.glDisable(GLES20.GL_DEPTH_TEST);
-
-            init();
+            super.onSurfaceCreated(gl, config);
         }
 
         @Override
         public void onSurfaceChanged(GL10 gl, int width, int height) {
             Log.d(TAG, "onSurfaceChanged");
-            GLES20.glViewport(0, 0, width, height);
+            super.onSurfaceChanged(gl, width, height);
             // Able to start preview now.
             Message msg = Message.obtain();
             msg.what = MSG_START_PREVIEW;
@@ -422,73 +315,23 @@ public class SharedEGLContextActivity extends Activity implements SurfaceTexture
 
         @Override
         public void onDrawFrame(GL10 gl) {
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
             mSurfaceTexture.updateTexImage();
-
-
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-            GLES20.glUseProgram(mProgram);
-
-            mCubeBuffer.position(0);
-            GLES20.glVertexAttribPointer(mAttributePosition, 2, GLES20.GL_FLOAT, false, 0, mCubeBuffer);
-            GLES20.glEnableVertexAttribArray(mAttributePosition);
-
-            mTextureBuffer.position(0);
-            GLES20.glVertexAttribPointer(mTextureCoords, 2, GLES20.GL_FLOAT, false, 0, mTextureBuffer);
-            GLES20.glEnableVertexAttribArray(mTextureCoords);
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-            GLES20.glUniform1i(mUniformLocation, 0);
-            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, mPreviewTexture);
-
-            float[] matrix = new float[16];
-            mSurfaceTexture.getTransformMatrix(matrix);
-            GLES20.glUniformMatrix4fv(mUniformMatrix, 1, false, matrix, 0);
-            final int error = GLES20.glGetError();
-            if (error != 0) {
-                Log.e("render", "render error " + String.format("0x%x", error));
-            }
-
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
-            GLES20.glDisableVertexAttribArray(mAttributePosition);
-            GLES20.glDisableVertexAttribArray(mTextureCoords);
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+            super.onDrawFrame(gl);
         }
 
-        public final void init() {
-            mProgram = loadProgram(VERTEX_SHADER, FRAGMENT_SHADER);
-            mAttributePosition = GLES20.glGetAttribLocation(mProgram, "position");
-            mTextureCoords = GLES20.glGetAttribLocation(mProgram, "inputTextureCoords");
-            mUniformLocation = GLES20.glGetUniformLocation(mProgram, "uTextureSampler");
-            mUniformMatrix = GLES20.glGetUniformLocation(mProgram, "uSTMatrix");
+        @Override
+        protected SurfaceTexture getSurfaceTexture() {
+            return mSurfaceTexture;
+        }
+
+        @Override
+        protected int getPreviewTexture() {
+            return mPreviewTexture;
         }
     }
 
-    private class FilterRenderer implements GLSurfaceView.Renderer {
+    private class FilterRenderer extends SurfaceTextureRenderer {
         private static final String TAG = "FilterRenderer";
-        public final float[] CUBE = {
-                -1.0f, -1.0f,
-                1.0f, -1.0f,
-                -1.0f, 1.0f,
-                1.0f, 1.0f,
-        };
-        public final float[] TEXTURE_NO_ROTATION = {
-                0.0f, 0.0f,
-                1.0f, 0.0f,
-                0.0f, 1.0f,
-                1.0f, 1.0f,
-        };
-        private final FloatBuffer mCubeBuffer;
-        private final FloatBuffer mTextureBuffer;
-
-        public static final String VERTEX_SHADER =
-                "attribute vec4 position;\n" +
-                        "uniform mat4 uSTMatrix;\n" +
-                        "attribute vec4 inputTextureCoords;\n" +
-                        "varying vec2 textureCoords;\n" +
-                        "void main() {\n" +
-                        "  gl_Position = position;\n" +
-                        "  textureCoords = (uSTMatrix * inputTextureCoords).xy;\n" +
-                        "}";
 
         public static final String GRAYSCALE_FRAGMENT_SHADER =
                 "#extension GL_OES_EGL_image_external : require\n" +
@@ -502,80 +345,19 @@ public class SharedEGLContextActivity extends Activity implements SurfaceTexture
                         "  gl_FragColor = vec4(gray, gray, gray, 1);\n" +
                         "}";
 
-        private int mProgram;
-        private int mAttributePosition;
-        protected int mTextureCoords;
-        private int mUniformLocation;
-        private int mUniformMatrix;
-
-        public FilterRenderer() {
-            mCubeBuffer = ByteBuffer.allocateDirect(CUBE.length * 4)
-                    .order(ByteOrder.nativeOrder())
-                    .asFloatBuffer();
-            mCubeBuffer.put(CUBE).position(0);
-
-            mTextureBuffer = ByteBuffer.allocateDirect(TEXTURE_NO_ROTATION.length * 4)
-                    .order(ByteOrder.nativeOrder())
-                    .asFloatBuffer();
-            mTextureBuffer.put(TEXTURE_NO_ROTATION).position(0);
+        @Override
+        protected SurfaceTexture getSurfaceTexture() {
+            return mSurfaceTexture;
         }
 
         @Override
-        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-            Log.d(TAG, "onSurfaceCreated");
-            // Initialize GL stuff
-            GLES20.glDisable(GLES20.GL_DITHER);
-            GLES20.glClearColor(0, 0, 0, 0);
-            GLES20.glEnable(GLES20.GL_CULL_FACE);
-            GLES20.glDisable(GLES20.GL_DEPTH_TEST);
-
-            init();
+        protected int getPreviewTexture() {
+            return mPreviewTexture;
         }
 
         @Override
-        public void onSurfaceChanged(GL10 gl, int width, int height) {
-            Log.d(TAG, "onSurfaceChanged");
-            GLES20.glViewport(0, 0, width, height);
-        }
-
-        @Override
-        public void onDrawFrame(GL10 gl) {
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-            GLES20.glUseProgram(mProgram);
-
-            mCubeBuffer.position(0);
-            GLES20.glVertexAttribPointer(mAttributePosition, 2, GLES20.GL_FLOAT, false, 0, mCubeBuffer);
-            GLES20.glEnableVertexAttribArray(mAttributePosition);
-
-            mTextureBuffer.position(0);
-            GLES20.glVertexAttribPointer(mTextureCoords, 2, GLES20.GL_FLOAT, false, 0, mTextureBuffer);
-            GLES20.glEnableVertexAttribArray(mTextureCoords);
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-            GLES20.glUniform1i(mUniformLocation, 0);
-            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, mPreviewTexture);
-
-            float[] matrix = new float[16];
-            mSurfaceTexture.getTransformMatrix(matrix);
-            GLES20.glUniformMatrix4fv(mUniformMatrix, 1, false, matrix, 0);
-            final int error = GLES20.glGetError();
-            if (error != 0) {
-                Log.e("render", "render error " + String.format("0x%x", error));
-            }
-
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
-            GLES20.glDisableVertexAttribArray(mAttributePosition);
-            GLES20.glDisableVertexAttribArray(mTextureCoords);
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
-        }
-
-        public final void init() {
-            mProgram = loadProgram(VERTEX_SHADER, GRAYSCALE_FRAGMENT_SHADER);
-            mAttributePosition = GLES20.glGetAttribLocation(mProgram, "position");
-            mTextureCoords = GLES20.glGetAttribLocation(mProgram, "inputTextureCoords");
-            mUniformLocation = GLES20.glGetUniformLocation(mProgram, "uTextureSampler");
-            mUniformMatrix = GLES20.glGetUniformLocation(mProgram, "uSTMatrix");
+        protected String getFragmentShader() {
+            return GRAYSCALE_FRAGMENT_SHADER;
         }
     }
 
@@ -600,117 +382,17 @@ public class SharedEGLContextActivity extends Activity implements SurfaceTexture
         }
     }
 
-    private class SketchRenderer implements GLSurfaceView.Renderer {
+    private class SketchRenderer extends SurfaceTextureRenderer {
         private static final String TAG = "SketchRenderer";
-        public final float[] CUBE = {
-                -1.0f, -1.0f,
-                1.0f, -1.0f,
-                -1.0f, 1.0f,
-                1.0f, 1.0f,
-        };
-        public final float[] TEXTURE_NO_ROTATION = {
-                0.0f, 0.0f,
-                1.0f, 0.0f,
-                0.0f, 1.0f,
-                1.0f, 1.0f,
-        };
-        private final FloatBuffer mCubeBuffer;
-        private final FloatBuffer mTextureBuffer;
 
-        public static final String VERTEX_SHADER =
-                "attribute vec4 position;\n" +
-                        "uniform mat4 uSTMatrix;\n" +
-                        "attribute vec4 inputTextureCoords;\n" +
-                        "varying vec2 textureCoords;\n" +
-                        "void main() {\n" +
-                        "  gl_Position = position;\n" +
-                        "  textureCoords = (uSTMatrix * inputTextureCoords).xy;\n" +
-                        "}";
-
-        public static final String FRAGMENT_SHADER =
-                "#extension GL_OES_EGL_image_external : require\n" +
-                        "precision highp float;\n" +
-                        "uniform samplerExternalOES uTextureSampler;\n" +
-                        "varying vec2 textureCoords;\n" +
-                        "void main () {\n" +
-                        "  vec4 tex = texture2D(uTextureSampler, textureCoords);\n" +
-                        "  gl_FragColor = vec4(tex.rgb, 1);\n" +
-                        "}";
-
-        private int mProgram;
-        private int mAttributePosition;
-        protected int mTextureCoords;
-        private int mUniformLocation;
-        private int mUniformMatrix;
-
-        public SketchRenderer() {
-            mCubeBuffer = ByteBuffer.allocateDirect(CUBE.length * 4)
-                    .order(ByteOrder.nativeOrder())
-                    .asFloatBuffer();
-            mCubeBuffer.put(CUBE).position(0);
-
-            mTextureBuffer = ByteBuffer.allocateDirect(TEXTURE_NO_ROTATION.length * 4)
-                    .order(ByteOrder.nativeOrder())
-                    .asFloatBuffer();
-            mTextureBuffer.put(TEXTURE_NO_ROTATION).position(0);
+        @Override
+        protected SurfaceTexture getSurfaceTexture() {
+            return mSurfaceTexture;
         }
 
         @Override
-        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-            Log.d(TAG, "onSurfaceCreated");
-            // Initialize GL stuff
-            GLES20.glDisable(GLES20.GL_DITHER);
-            GLES20.glClearColor(0, 0, 0, 0);
-            GLES20.glEnable(GLES20.GL_CULL_FACE);
-            GLES20.glDisable(GLES20.GL_DEPTH_TEST);
-
-            init();
-        }
-
-        @Override
-        public void onSurfaceChanged(GL10 gl, int width, int height) {
-            Log.d(TAG, "onSurfaceChanged");
-            GLES20.glViewport(0, 0, width, height);
-        }
-
-        @Override
-        public void onDrawFrame(GL10 gl) {
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-            GLES20.glUseProgram(mProgram);
-
-            mCubeBuffer.position(0);
-            GLES20.glVertexAttribPointer(mAttributePosition, 2, GLES20.GL_FLOAT, false, 0, mCubeBuffer);
-            GLES20.glEnableVertexAttribArray(mAttributePosition);
-
-            mTextureBuffer.position(0);
-            GLES20.glVertexAttribPointer(mTextureCoords, 2, GLES20.GL_FLOAT, false, 0, mTextureBuffer);
-            GLES20.glEnableVertexAttribArray(mTextureCoords);
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-            GLES20.glUniform1i(mUniformLocation, 0);
-            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, mPreviewTexture);
-
-            float[] matrix = new float[16];
-            mSurfaceTexture.getTransformMatrix(matrix);
-            GLES20.glUniformMatrix4fv(mUniformMatrix, 1, false, matrix, 0);
-            final int error = GLES20.glGetError();
-            if (error != 0) {
-                Log.e("render", "render error " + String.format("0x%x", error));
-            }
-
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
-            GLES20.glDisableVertexAttribArray(mAttributePosition);
-            GLES20.glDisableVertexAttribArray(mTextureCoords);
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
-        }
-
-        public final void init() {
-            mProgram = loadProgram(VERTEX_SHADER, FRAGMENT_SHADER);
-            mAttributePosition = GLES20.glGetAttribLocation(mProgram, "position");
-            mTextureCoords = GLES20.glGetAttribLocation(mProgram, "inputTextureCoords");
-            mUniformLocation = GLES20.glGetUniformLocation(mProgram, "uTextureSampler");
-            mUniformMatrix = GLES20.glGetUniformLocation(mProgram, "uSTMatrix");
+        protected int getPreviewTexture() {
+            return mPreviewTexture;
         }
     }
 }
---
+++ app/src/main/java/net/toughcoder/opengl/sharedcontext/SurfaceTextureRenderer.java
@@ -0,0 +1,188 @@
+package net.toughcoder.opengl.sharedcontext;
+
+import android.graphics.SurfaceTexture;
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by alex on 17-9-16.
+ */
+
+abstract class SurfaceTextureRenderer implements GLSurfaceView.Renderer {
+    private static final String TAG = "PreviewRenderer";
+    public final float[] CUBE = {
+            -1.0f, -1.0f,
+            1.0f, -1.0f,
+            -1.0f, 1.0f,
+            1.0f, 1.0f,
+    };
+    public final float[] TEXTURE_NO_ROTATION = {
+            0.0f, 0.0f,
+            1.0f, 0.0f,
+            0.0f, 1.0f,
+            1.0f, 1.0f,
+    };
+    private final FloatBuffer mCubeBuffer;
+    private final FloatBuffer mTextureBuffer;
+
+    private static final String VERTEX_SHADER =
+            "attribute vec4 position;\n" +
+                    "uniform mat4 uSTMatrix;\n" +
+                    "attribute vec4 inputTextureCoords;\n" +
+                    "varying vec2 textureCoords;\n" +
+                    "void main() {\n" +
+                    "  gl_Position = position;\n" +
+                    "  textureCoords = (uSTMatrix * inputTextureCoords).xy;\n" +
+                    "}";
+
+    private static final String FRAGMENT_SHADER =
+            "#extension GL_OES_EGL_image_external : require\n" +
+                    "precision highp float;\n" +
+                    "uniform samplerExternalOES uTextureSampler;\n" +
+                    "varying vec2 textureCoords;\n" +
+                    "void main () {\n" +
+                    "  vec4 tex = texture2D(uTextureSampler, textureCoords);\n" +
+                    "  gl_FragColor = vec4(tex.rgb, 1);\n" +
+                    "}";
+
+    private int mProgram;
+    private int mAttributePosition;
+    protected int mTextureCoords;
+    private int mUniformLocation;
+    private int mUniformMatrix;
+
+    public SurfaceTextureRenderer() {
+        mCubeBuffer = ByteBuffer.allocateDirect(CUBE.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        mCubeBuffer.put(CUBE).position(0);
+
+        mTextureBuffer = ByteBuffer.allocateDirect(TEXTURE_NO_ROTATION.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        mTextureBuffer.put(TEXTURE_NO_ROTATION).position(0);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        Log.d(TAG, "onSurfaceCreated");
+
+        // Initialize GL stuff
+        GLES20.glDisable(GLES20.GL_DITHER);
+        GLES20.glClearColor(0, 0, 0, 0);
+        GLES20.glEnable(GLES20.GL_CULL_FACE);
+        GLES20.glDisable(GLES20.GL_DEPTH_TEST);
+
+        init();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        Log.d(TAG, "onSurfaceChanged");
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUseProgram(mProgram);
+
+        mCubeBuffer.position(0);
+        GLES20.glVertexAttribPointer(mAttributePosition, 2, GLES20.GL_FLOAT, false, 0, mCubeBuffer);
+        GLES20.glEnableVertexAttribArray(mAttributePosition);
+
+        mTextureBuffer.position(0);
+        GLES20.glVertexAttribPointer(mTextureCoords, 2, GLES20.GL_FLOAT, false, 0, mTextureBuffer);
+        GLES20.glEnableVertexAttribArray(mTextureCoords);
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glUniform1i(mUniformLocation, 0);
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, getPreviewTexture());
+
+        float[] matrix = new float[16];
+        getSurfaceTexture().getTransformMatrix(matrix);
+        GLES20.glUniformMatrix4fv(mUniformMatrix, 1, false, matrix, 0);
+        final int error = GLES20.glGetError();
+        if (error != 0) {
+            Log.e("render", "render error " + String.format("0x%x", error));
+        }
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+        GLES20.glDisableVertexAttribArray(mAttributePosition);
+        GLES20.glDisableVertexAttribArray(mTextureCoords);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+    }
+
+    public final void init() {
+        mProgram = loadProgram(getVertexShader(), getFragmentShader());
+        mAttributePosition = GLES20.glGetAttribLocation(mProgram, "position");
+        mTextureCoords = GLES20.glGetAttribLocation(mProgram, "inputTextureCoords");
+        mUniformLocation = GLES20.glGetUniformLocation(mProgram, "uTextureSampler");
+        mUniformMatrix = GLES20.glGetUniformLocation(mProgram, "uSTMatrix");
+    }
+
+    protected abstract SurfaceTexture getSurfaceTexture();
+
+    protected abstract int getPreviewTexture();
+
+    protected String getFragmentShader() {
+        return FRAGMENT_SHADER;
+    }
+
+    protected String getVertexShader() {
+        return VERTEX_SHADER;
+    }
+
+    public static int loadProgram(final String vsh, final String fsh) {
+        int vshader = loadShader(vsh, GLES20.GL_VERTEX_SHADER);
+        if (vshader == 0) {
+            Log.e(TAG, "failed to load vertex shader " + vsh);
+            return 0;
+        }
+
+        int fshader = loadShader(fsh, GLES20.GL_FRAGMENT_SHADER);
+        if (fshader == 0) {
+            Log.e(TAG, "failed to load fragment shader " + fsh);
+            return 0;
+        }
+
+        int program = GLES20.glCreateProgram();
+        GLES20.glAttachShader(program, vshader);
+        GLES20.glAttachShader(program, fshader);
+        GLES20.glLinkProgram(program);
+
+        int[] status = new int[1];
+        GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, status, 0);
+        if (status[0] <= 0) {
+            Log.e(TAG, "failed to link program");
+        }
+
+        GLES20.glDeleteShader(vshader);
+        GLES20.glDeleteShader(fshader);
+
+        return program;
+    }
+
+    public static int loadShader(String source, int type) {
+        int[] status = new int[1];
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, source);
+        GLES20.glCompileShader(shader);
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, status, 0);
+        if (status[0] == 0) {
+            Log.e(TAG, "failed to compile shader " + source + ", type " + type);
+            return 0;
+        }
+        return shader;
+    }
+}
