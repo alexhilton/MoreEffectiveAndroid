04eb7afd0da15a19dd8a6cf6a14312b79f3b532f
Alex Hilton <dragonhilton@gmail.com>
2017-10-05 14:31:04 +0800

    Get notified for Surface.
    Signed-off-by: Alex Hilton <dragonhilton@gmail.com>

--- app/src/main/java/net/toughcoder/opengl/miniglview/OpenGLESView.java
+++ app/src/main/java/net/toughcoder/opengl/miniglview/OpenGLESView.java
@@ -1,8 +1,46 @@
 package net.toughcoder.opengl.miniglview;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
 /**
  * Created by alex on 10/2/17.
  */
 
-public class OpenGLESView {
+public class OpenGLESView extends SurfaceView implements SurfaceHolder.Callback {
+    public OpenGLESView(Context context) {
+        super(context);
+        init();
+    }
+
+    public OpenGLESView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public OpenGLESView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
 }
